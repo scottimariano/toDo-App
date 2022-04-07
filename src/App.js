00891a7b4e52db773e0e4028a40e109723e0ffb0
scottimariano <32655578+scottimariano@users.noreply.github.com>
2022-04-07 17:14:42 -0300

    Beggining with wooks implementation / addTodo not working

--- src/App.js
+++ src/App.js
@@ -1,70 +1,70 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Tareas from './componentes/Tareas';
 import NavBar from './componentes/NavBar';
 import {Routes, Route} from "react-router-dom";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      toDos: [],
-      //toDos: [{id:0, name: 'Task 1', descripcion: 'Description Task 1 test', completed: false}, {id:1, name: 'Task 2', descripcion: 'Description Task 2 test',completed: false}, {id:2, name: 'Task 3', descripcion: 'Description Task 3 test', completed: true}],
-      
-      operations: {
-        handleChangeCompleted : (i) => {
-          let newtoDos = this.state.toDos;
-          const index = newtoDos.findIndex(toDo => {return toDo.id === i})
-          newtoDos[index].completed = !newtoDos[index].completed;
-          this.setState({toDos: newtoDos})
-        },
+function App() {
+  let [toDos, setTodos] = useState([]);
+
+  const operations = {   
+    handleChangeCompleted : (i) => {
+      let newToDos = toDos;
+      const index = newToDos.findIndex(toDo => {return toDo.id === i})
+      newToDos[index].completed = !newToDos[index].completed;
+      setTodos(toDos = newToDos)
+    },
         
-        addTodo : (data) => {
-          let newTodos = this.state.toDos;
-          newTodos.push(data);
-          this.setState({toDos : newTodos})
-        }, 
+    addTodo : (data) => {
+      let nuevoToDo= toDos;
+      nuevoToDo.push(data)
+      setTodos(nuevoToDo)
+    },
 
-        deleteTodo : (id) => {
-          let newTodos = this.state.toDos;
-          const filteredTodos = newTodos.filter(toDo => toDo.id !== id )
-          this.setState({toDos : filteredTodos})
-        }
-
-      }
+    deleteTodo : (id) => {
+      let newToDos = toDos;
+      const filteredTodos = newToDos.filter(toDo => toDo.id !== id )
+      setTodos(toDos = filteredTodos)
     }
-    
   }
+  
 
-  cargarToDos() {
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     toDos: [],
+  //     //toDos: [{id:0, name: 'Task 1', descripcion: 'Description Task 1 test', completed: false}, {id:1, name: 'Task 2', descripcion: 'Description Task 2 test',completed: false}, {id:2, name: 'Task 3', descripcion: 'Description Task 3 test', completed: true}],
+      
+  //       
+  //     }
+  //   }
+  // }
+
+  function cargarToDos() {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then((response) => response.json())
       .then((json) => {
-        this.setState({toDos : json.slice(0,5)})
-        console.log(this.state.toDos)
+        setTodos(json.slice(0,5)) // solo cargamos los primeros 5 de jsonplaceholder (que son 100!)
       });
-
   }
 
-  componentDidMount() {
-    this.cargarToDos()
+  useEffect(() => {
+    cargarToDos()
+  },[])
+     
+  return (
+    <div class="container">
+      <header>
+        <NavBar />    
+      </header>
+      <Routes>
+        <Route path='/tasks' element={<Tareas toDos={toDos} operations={operations}/>} />
+        <Route path='/notes' element={<h1>NOTES</h1>} />
+        <Route path='/contacts' element={<h1>CONTACTS</h1>} />
+        <Route path='/' element={<h1>HOMEPAGE</h1>}/>
+      </Routes>
+    </div>
+  );
 
-  }
-
-  render () {
-    return (
-      
-      <div class="container">
-        <header>
-          <NavBar />    
-        </header>
-        <Routes>
-          <Route path='/tasks' element={<Tareas toDos={this.state.toDos} operations={this.state.operations}/>} />
-          <Route path='/notes' element={<h1>NOTES</h1>} />
-          <Route path='/contacts' element={<h1>CONTACTS</h1>} />
-        </Routes>
-      </div>
-    );
-  }
 }
 
 export default App;
